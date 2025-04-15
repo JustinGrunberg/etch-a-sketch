f10e4b494c75acb5d982b4debed07930eb63d157
JustinGrunberg <j.e.grunberg@gmail.com>
2025-04-15 12:12:41 +0200

    feat: Add event handler to create new grids

--- javascript.js
+++ javascript.js
@@ -1,12 +1,39 @@
 let container = document.querySelector('#container')
+let gridCreator = document.querySelector('#grid-creator')
+
+const CONTAINER_WIDTH = 512
+
+gridCreator.addEventListener('click', (e) => {
+    let userInput = prompt('Create a new Grid! What size do you want? (max. 100)')
+    let gridSize = parseInt(userInput)
+
+    if(!userInput){
+        return
+    }
+    else if(isNaN(gridSize) || gridSize < 1 || gridSize > 100){
+        alert('Input invalid! Chose a number between 0 - 100')
+        return
+    } 
+
+    let totalGridSize = gridSize * gridSize
+    let cellSize = CONTAINER_WIDTH / gridSize
+
+    console.log(cellSize)
+
+    container.innerHTML = ''
+    createGrid(totalGridSize, cellSize)
+    
+})
 container.addEventListener('mouseover', createHoverEffect)
 
-let gridSize = 16 * 16
+let totalGridSize = 16 * 16
 
-function createGrid(){
-    for(let i = 0; i < gridSize; i++){
+function createGrid(totalGridSize, cellSize){
+    for(let i = 0; i < totalGridSize; i++){
         let div = document.createElement('div')
         div.className = 'grid-item'
+        div.style.width = `${cellSize}px`
+        div.style.height = `${cellSize}px`
         container.appendChild(div)
     }
 }
@@ -17,4 +44,4 @@ function createHoverEffect(e){
     }
 }
 
-createGrid()
+createGrid(totalGridSize, 32)
